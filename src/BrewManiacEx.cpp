e15b1afbdfa79a40c54921dda68d9ed4138b99be
Vito Tai <vitotai@mail.com>
2022-03-15 16:00:41 +0800

    issue #103

--- src/BrewManiacEx.cpp
+++ src/BrewManiacEx.cpp
@@ -1180,8 +1180,7 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
     	if(info->final && info->index == 0 && info->len == len){
       		//the whole message is in a single frame and we got all of it's data
       		DBG_PRINTF("ws[%s][%u] %s-message[%llu]\n", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
-			
-			processRemoteCommand(client,data,info->len);
+			if(info->opcode ==WS_TEXT && info->len>0) processRemoteCommand(client,data,info->len);
 
 		} else {
       		//message is comprised of multiple frames or the frame is split into multiple packets
